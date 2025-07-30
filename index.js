ffdeb159ab0c5a72544f2712a6c182cbba2ee84c
Justin <justinsmith200207@gmail.com>
2025-07-30 11:42:51 -0400

    Graph shows on screen

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const app = document.getElementById("app")
 
+var savings = {}
+
 function main(){
     createUpload()
+    createGraph()
+}
+
+function createGraph(){
+    var canvas = document.createElement("canvas")
+    app.appendChild(canvas)
+    canvas = canvas.getContext("2d")
+
+    const graph = new Chart(canvas, {
+            type: 'line',
+            data: {
+                labels: Object.keys(savings),
+                datasets: [{
+                    label: 'Savings ($)',
+                    data: Object.values(savings),
+                    borderColor: 'rgb(75, 192, 192)',
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    tension: 0.3
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
 }
 
 function createUpload(){
